41ef7e9b7eee618d2fa7fcd5ccc1fa12f4545c18
cadumeneses <carloseduardoms646@gmail.com>
2022-04-12 06:32:58 -0300

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
-import { navBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './error-404/error-404.component';
-import { CourseInfoComponent } from './courses/course-info.component';
-import { Course } from './courses/course';
+import { CourseModule } from './courses/course.module';
+import { CoreModule } from './Core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
-    navBarComponent,
     Error404Component,
-    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    CourseModule,
+    CoreModule,
     HttpClientModule,
     RouterModule.forRoot([
-      {
-        path: 'courses', component:   CourseListComponent
-      },
-      {
-        path: 'course/info/:id', component: CourseInfoComponent
-      },
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
       },
